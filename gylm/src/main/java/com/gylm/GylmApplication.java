bda159ecd35b840066010469552ced6e91684cf7
wangtao005 <wangtao005@hotmail.com>
2019-09-17 19:58:24 +0800

    1

---
+++ gylm/src/main/java/com/gylm/GylmApplication.java
@@ -0,0 +1,14 @@
+package com.gylm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GylmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GylmApplication.class, args);
+		System.out.println("-------------启动成功---------------");
+	}
+
+}
---
+++ gylm/src/test/java/com/gylm/GylmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gylm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GylmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
