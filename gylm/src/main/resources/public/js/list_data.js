675f61634990f45e882fd546ff18f97526f7d8e9
wt <wangtao9009>
2020-01-02 15:49:26 +0800

    1

--- gylm/src/main/resources/public/js/list_data.js
+++ gylm/src/main/resources/public/js/list_data.js
@@ -11,7 +11,7 @@ $(function(){
 		      offset: hh,
 			// area: ['500px', '360px'],
 		      shadeClose: true, //点击遮罩关闭
-		      content: '\<\div style="padding:20px;"  align="middle"><img src="..\/image\/183weixincode.jpg" style="height: 150px;width: 150px;"><\/br><H2 style="padding:40px;" >请使用微信扫描二维码添加客服咨询<\/H2>     \<\/div>'
+		      content: '\<\div style="padding:20px;"  align="middle"><img src="../../image\/183weixincode.jpg" style="height: 150px;width: 150px;"><\/br><H2 style="padding:40px;" >请使用微信扫描二维码添加客服咨询<\/H2>     \<\/div>'
 		    });
 		  });
     appendNewList(1,pageSize);
