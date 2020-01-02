a5102dfed45a6db82254295735b1a195c17ad3ae
wt <wangtao9009>
2020-01-02 10:54:17 +0800

    1

--- gylm/src/main/resources/public/js/myjs.js
+++ gylm/src/main/resources/public/js/myjs.js
@@ -14,14 +14,9 @@ $(function(){
 		      content: '\<\div style="padding:20px;"  align="middle"><img src="..\/image\/183weixincode.jpg" style="height: 150px;width: 150px;"><\/br><H2 style="padding:40px;" >请使用微信扫描二维码添加客服咨询<\/H2>     \<\/div>'
 		    });
 		  });
-    appendNewList();
+    appendNewList(1,pageSize);
 });
 var new_list=[];
-function appendNewList(){
-    for (var i=0;i<new_list.length;i++) {
-        $("#new_list").append(new_list[i]);
-    }
-}
 var new_list_info =[
          {  url:"/37.html",
             title:"高佣联盟4.1版本功能",
@@ -32,12 +27,8 @@ var new_list_info =[
             title:"高佣联盟4.2版本功能",
             date:"2019-12-23",
             info:"高佣联盟4.0版本功能介绍，本次版本的升级主要有个5比较大的改动和大家同步这几个改动都是和大家的使用息息相关的，首先和大家说说首页的改版内容"
-        },
-         {  url:"/37.html",
-            title:"高佣联盟4.3版本功能",
-            date:"2019-12-23",
-            info:"高佣联盟4.0版本功能介绍，本次版本的升级主要有个5比较大的改动和大家同步这几个改动都是和大家的使用息息相关的，首先和大家说说首页的改版内容"
         }
+
 ];
 
 for (var i=0;i<new_list_info.length;i++) {
@@ -55,18 +46,33 @@ for (var i=0;i<new_list_info.length;i++) {
             '</article>';
         new_list.push(new_1);
 }
-var total = Math.ceil(new_list/10);
+var pageSize = 10;
+var total = Math.ceil(new_list.length/pageSize);
 $(function() {
     $("#pagination").pagination({
         currentPage: 1,
         totalPage: total,
         callback: function(current) {
-            $("#current1").text(current)
+            // $("#current1").text(current);
+            $("#new_list").empty();
+            appendNewList(current,pageSize);
         }
     });
 
 });
-
+function appendNewList(page,pageSize){
+    if (!page){
+        i=page-1;
+    } else{
+        i=(page-1)*pageSize;
+    }
+    var mt = pageSize;
+    for ( i;i<new_list.length;i++) {
+        if(mt==0){  break; }
+        $("#new_list").append(new_list[i]);
+        mt--;
+    }
+}
 
 
 
