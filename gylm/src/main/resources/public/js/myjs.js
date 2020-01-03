aa8e166bf11017c43fe5558b36a4b9c174d0ad4f
wt <wangtao9009>
2020-01-03 16:54:42 +0800

    1

--- gylm/src/main/resources/public/js/myjs.js
+++ gylm/src/main/resources/public/js/myjs.js
@@ -15,7 +15,11 @@ $(function(){
 		    });
 		  });
 });
-
+//360代码自动提交代码
+(function(){
+    var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+    document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
 
 
 
