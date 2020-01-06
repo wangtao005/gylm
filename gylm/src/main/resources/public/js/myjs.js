5c32dd028f3a63049a023a1a8311925d99e3e0ba
wt <wangtao9009>
2020-01-06 09:52:26 +0800

    1

--- gylm/src/main/resources/public/js/myjs.js
+++ gylm/src/main/resources/public/js/myjs.js
@@ -1,4 +1,5 @@
 $(function(){
+    var baseUrl1 = window.location.origin;
 	$('#ask').on('click', function(){
 			  var h = document.body.clientHeight;
 				 var hh = h/8;
@@ -11,7 +12,7 @@ $(function(){
 		      offset: hh,
 			// area: ['500px', '360px'],
 		      shadeClose: true, //点击遮罩关闭
-		      content: '\<\div style="padding:20px;"  align="middle"><img src="..\/image\/183weixincode.jpg" style="height: 150px;width: 150px;"><\/br><H2 style="padding:40px;" >请使用微信扫描二维码添加客服咨询<\/H2>     \<\/div>'
+		      content: '\<\div style="padding:20px;"  align="middle"><img src="'+baseUrl1+'/image\/183weixincode.jpg" style="height: 150px;width: 150px;"><\/br><H2 style="padding:40px;" >请使用微信扫描二维码添加客服咨询<\/H2>     \<\/div>'
 		    });
 		  });
 });
